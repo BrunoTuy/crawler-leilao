3a67cbffd8d5a9ce6c1cda3951ceeb5feddddd1e
Bruno Tuy <murta@tuy.com.br>
2022-12-10 00:33:28 -0300

    schedules

--- index.js
+++ index.js
@@ -8,42 +8,54 @@ const db = await _db();
 const palacio = await p({ cheerio, request, db });
 const { close, buscarLista, atualizarRegistro } = db;
 
-const buscarLotesSalvar = async() => {
+const buscarLotesSalvar = async () => {
   await palacio.buscarLotesSalvar();
 };
 
-const buscarLotesAtualizar = async (tempoEntreRequisicoes) => {
-  const listaBanco = await buscarLista('palacioDosLeiloes', true);
-
-  baixarLoteAtualizar('palacioDosLeiloes', listaBanco, 0, tempoEntreRequisicoes);
-};
-
-const baixarLoteAtualizar = async (collection, array, index, tempoEntreRequisicoes) => {
-  if (index > array.length - 1) {
-    console.log(`Fim da lista - ${index+1}/${array.length}`);
-
-    return close();
-  }
-
-  const { registro } = array[index];
-
-  console.log(registro, `${index+1}/${array.length}`, 'Vamos atualizar');
-
-  const informacoesSite = await palacio.verLotes(registro);
-
-  await atualizarRegistro(collection, {
-    registro,
-    ...informacoesSite
+const buscarLotesAtualizar = async () => {
+  await palacio.buscarLotesAtualizar({
+    filtroHoras: '+6',
+    tempoEntreRequisicoes: 30000
   });
+}
 
-  setTimeout(() => baixarLoteAtualizar(collection, array, index+1, tempoEntreRequisicoes), tempoEntreRequisicoes);
-};
+cron.schedule('*/3 * * * *', async () => {
+  console.log('*** Atualizando lotes 2', new Date());
+  await palacio.buscarLotesAtualizar({
+    filtroHoras: '2',
+    tempoEntreRequisicoes: 3000
+  });
+  console.log('*** Finalizando atualização lotes 2', new Date());
+}, {
+  scheduled: true,
+  timezone: "America/Sao_Paulo"
+});
 
+cron.schedule('*/30 * * * *', async () => {
+  console.log('*** Atualizando lotes 6', new Date());
+  await palacio.buscarLotesAtualizar({
+    filtroHoras: '6',
+    tempoEntreRequisicoes: 10000
+  });
+  console.log('*** Finalizando atualização lotes 6', new Date());
+}, {
+  scheduled: true,
+  timezone: "America/Sao_Paulo"
+});
 
-// buscarLotesAtualizar(2000);
-// buscarLotesSalvar();
+cron.schedule('21 3,18 * * *', async () => {
+  console.log('*** Atualizando lotes +6', new Date());
+  await palacio.buscarLotesAtualizar({
+    filtroHoras: '+6',
+    tempoEntreRequisicoes: 30000
+  });
+  console.log('*** Finalizando atualização lotes +6', new Date());
+}, {
+  scheduled: true,
+  timezone: "America/Sao_Paulo"
+});
 
-cron.schedule('*/15 * * * *', async () => {
+cron.schedule('19 1 * * *', async () => {
   console.log('*** Buscar lotes', new Date());
   await buscarLotesSalvar();
   console.log('*** Finalizando busca de lotes', new Date());
