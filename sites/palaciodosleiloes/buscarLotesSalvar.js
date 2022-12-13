376f76584d4b85e18dc6517cd4650f8777cd2da0
Bruno <bruno.tuy@pixeon.com>
2022-12-13 07:33:13 -0300

    funcao retirada de db

--- sites/palaciodosleiloes/buscarLotesSalvar.js
+++ sites/palaciodosleiloes/buscarLotesSalvar.js
@@ -2,11 +2,48 @@ import ll from './listarLotes.js';
 
 const exec = (params) => {
   const listarLotes = ll(params);
-  const { salvarLista } = params.db;
+  const { buscarLista, insert, update } = params.db;
+
+  const salvarLista = async (listaSite) => {
+    const colecao = 'palacioDosLeiloes';
+    const listaBanco = await buscarLista({ colecao });
+
+    listaSite.forEach(async (i, index, array) => {
+      const itemBanco = listaBanco.find(({ registro }) => registro.lote === i.registro.lote && registro.leilao === i.registro.leilao);
+
+      if (itemBanco) {
+        const set = {};
+
+        if (i.previsao.time && JSON.stringify(i.previsao) != JSON.stringify(itemBanco.previsao)) {
+          set.previsao = i.previsao;
+        }
+
+        if (i.sequencia && JSON.stringify(i.sequencia) != JSON.stringify(itemBanco.sequencia)) {
+          set.sequencia = i.sequencia;
+        }
+
+        if (i.lote && JSON.stringify(i.lote) != JSON.stringify(itemBanco.lote)) {
+          set.lote = i.lote;
+        }
+
+        if (set !== {}) {
+          const atualizado = await update({ colecao, registro: i.registro, set });
+
+          console.log(colecao, i.registro, `Registro ${atualizado ? '' : 'não '}atualizado`);
+        } else {
+          console.log(colecao, i.registro, 'Registro já cadastrado');
+        }
+      } else {
+        const id = await insert({ colecao, dados: i });
+
+        console.log(colecao, i.registro, `${id ? '' : 'Não '}Cadastrado`, id);
+      }
+    });
+  };
 
   const fnc = async () => {
     const listaSite = await listarLotes();
-    await salvarLista('palacioDosLeiloes', listaSite);
+    await salvarLista(listaSite);
   };
 
   return fnc;
