90a70b4a858781009979c67bc9a67f3d7d73ffb1
Bruno <bruno.tuy@pixeon.com>
2022-12-12 22:02:06 -0300

    tratar data com ano de 2 digitos

--- sites/palaciodosleiloes/_tratarDataHora.js
+++ sites/palaciodosleiloes/_tratarDataHora.js
@@ -6,15 +6,15 @@ const tratarDataHora = (val) => {
 
     obj.date = new Date(data);
     obj.time = obj.date.getTime();
-  } else if (val.length === 19 && val.includes('/') && val.includes(':')) {
+  } else if ((val.length === 19 || val.length === 17) && val.includes('/') && val.includes(':')) {
     const array = val.split(' ');
     const arrayData = array[0].split('/');
-    const data = `${arrayData[2]}-${arrayData[1]}-${arrayData[0]}`;
+    const data = `${arrayData[2].length == 2 ? '20' : ''}${arrayData[2]}-${arrayData[1]}-${arrayData[0]}`;
     const hora = array[1];
 
     obj.date = new Date(`${data}T${hora}`);
     obj.time = obj.date.getTime();
-   }
+  }
 
   return obj;
 };
