6ed436d8b92c44363b86f3fc3abe548758af8a58
Bruno Tuy <murta@tuy.com.br>
2023-01-07 23:12:13 -0300

    ajuste cron

--- milanleiloes.js
+++ milanleiloes.js
@@ -1,9 +1,25 @@
 import request from 'request-promise';
 import cheerio from 'cheerio';
+import cron from 'node-cron';
 import m from './sites/milanleiloes/index.js';
 import _db from './db.js';
 
 const db = await _db();
 const milian = await m({ cheerio, request, db });
 
-milian.buscarListaVeiculos();
+
+cron.schedule('00 12,19 * * *', async () => {
+  console.log('*** Atualizando lista de lotes', new Date());
+	milian.buscarListaVeiculos();
+}, {
+  scheduled: true,
+  timezone: "America/Sao_Paulo"
+});
+
+cron.schedule('45 */3 * * *', async () => {
+  console.log('*** Atualizando lotes', new Date());
+	milian.atualizarLote();
+}, {
+  scheduled: true,
+  timezone: "America/Sao_Paulo"
+});
