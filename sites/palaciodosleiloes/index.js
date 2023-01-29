bdc2d515602bb4095dcaaed83b2ec8cdac3f3cab
Bruno <bruno.tuy@pixeon.com>
2023-01-29 12:22:21 -0300

    retirando funcao desnecessaria

--- sites/palaciodosleiloes/index.js
+++ sites/palaciodosleiloes/index.js
@@ -1,13 +1,9 @@
-import listarLotes from './listarLotes.js';
-import verLotes from './verLotes.js';
 import buscarLotesSalvar from './buscarLotesSalvar.js'
 import buscarLotesAtualizar from './buscarLotesAtualizar.js';
 import downloadFotos from './downloadFotos.js';
 import migrarDados from './migrarDados.js';
 
 export default (params) => ({
-  listarLotes: listarLotes(params),
-  verLotes: verLotes(params),
   buscarLotesSalvar: buscarLotesSalvar(params),
   buscarLotesAtualizar: buscarLotesAtualizar(params),
   downloadFotos: downloadFotos(params),
