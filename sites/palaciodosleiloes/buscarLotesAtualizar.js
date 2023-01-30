4daf3a250c2c721f5f170e1bfd3a972af47d69fd
Bruno <bruno.tuy@pixeon.com>
2023-01-30 07:35:09 -0300

    cadastrando e atualizando direto em veiculos

--- sites/palaciodosleiloes/buscarLotesAtualizar.js
+++ sites/palaciodosleiloes/buscarLotesAtualizar.js
@@ -1,98 +1,178 @@
-import vl from './verLotes.js';
+import tratarDinheiro from './_tratarDinheiro.js';
+import tratarDataHora from './_tratarDataHora.js';
 
-const exec = (params) => {
-  const verLotes = vl(params);
-  const { close, buscarLista, update, get } = params.db;
+const pegarDeParenteses = (val) => {
+  const inicio = val.indexOf('(')+1;
+  const fim = val.indexOf(')');
 
-  const atualizarRegistro = async (colecao, informacoesSite) => {
-    const i = await get({ colecao, registro: informacoesSite.registro });
+  return val.substring(inicio, fim);
+};
 
-    if (!i) {
-      console.log(colecao, informacoesSite.registro, 'Não cadastrado');
-      return false;
+const dadosItem = (dados) => {
+  const {
+    registro,
+    encerrado,
+    bem: veiculo,
+    origem: vendedor,
+    Locallote: localLote,
+    LocalLeilao: localLeilao,
+    realizacao: dataInicio,
+    Acessorios,
+    Ano: ano,
+    Combustivel: combustivel,
+    Situacao: situacao,
+    lances,
+    origemTipo: vendedorTipo,
+    KM,
+    descricao,
+    fotos,
+  } = dados;
+
+  delete(dados._id);
+  delete(dados.log);
+
+  const {
+    data: ultimoLanceData,
+    valor: ultimoLanceValor
+  } = (lances || [{}]).pop();
+
+  const retorno = {};
+  const objeto = {
+    registro,
+    vendedor,
+    vendedorTipo,
+    veiculo,
+    combustivel,
+    ano,
+    km: isNaN(KM) ? KM : Number(KM.replace('.', '')),
+    situacao,
+    acessorios: (Acessorios || '').split(','),
+    descricao,
+    fotos,
+    ultimoLanceData,
+    ultimoLanceValor,
+    localLote,
+    localLeilao,
+    dataInicio,
+    encerrado,
+    original: dados
+  };
+
+  Object.entries(objeto).forEach(([key, value]) => {
+    if (value) {
+      retorno[key] = value;
     }
+  });
 
-    const setDados = {};
+  return retorno;
+}
 
-    if (informacoesSite.encerrado) {
-      if (i.encerrado === true) {
-        console.log(i.registro, 'Registro já encerrado');
-      } else if (!isNaN(i.encerrado)) {
-        setDados.encerrado = ++i.encerrado;
-
-        if (setDados.encerrado > 5) {
-          setDados.encerrado = true;
-        }
-      } else {
-        setDados.encerrado = 1;
+const exec = ({ cheerio, request, db: { list, get, salvarLista } }) => {
+  const colecao = 'veiculos';
+  const site = 'palaciodosleiloes.com.br';
+  const baixarPagina = async ({ lote, leilao }) => {
+    const response = await request.post('https://www.palaciodosleiloes.com.br/camada_ajax/lotem.php', {
+      form: {
+        opcao: "exibir_lote_m",
+        cod_lote: lote,
+        cod_leilao: leilao
       }
-    } else {
-      if (i.encerrado) {
-        setDados.encerrado = 0;
-      }
-
-      Object.entries(informacoesSite)
-        .filter(([key]) => !['_id', 'lances', 'log', 'registro', 'fotos'].includes(key))
-        .forEach(([key, value]) => {
-          if (key && JSON.stringify(i[key]) != JSON.stringify(value)) {
-            setDados[key] = value;
-          }
-        });
-
-      const lancesSite = [];
-      (informacoesSite.lances || []).forEach(l => {
-        if (!i.lances || !i.lances.push) {
-          i.lances = [];
-        }
-
-        if (!i.lances || !i.lances.push || !i.lances.find(({ valor }) => valor === l.valor)) {
-          lancesSite.push(l);
-        }
-      });
-
-      const fotosSite = [];
-      (informacoesSite.fotos || []).forEach(f => {
-        if (!i.fotos || !i.fotos.push) {
-          i.fotos = [];
-        }
-
-        if (!i.fotos.find(({ url }) => url === f)) {
-          fotosSite.push({ url: f });
-        }
-      });
-
-      if (lancesSite && lancesSite.length > 0 && i.lances && i.lances.push) {
-        setDados.lances = i.lances.concat(lancesSite);
-      }
-
-      if (fotosSite && fotosSite.length > 0 && i.fotos && i.fotos.push) {
-        setDados.fotos = i.fotos.concat(fotosSite);
-      }
-    }
-
-    if (JSON.stringify(setDados) != '{}') {
-      const atualizado = await update({ colecao, registro: i.registro, set: setDados });
-
-      console.log(colecao, i.registro, `Registro ${atualizado ? '' : 'não '}atualizado`);
-    } else {
-      console.log(i.registro, 'Registro sem atualizações');
-    }
-  }
-
-  const buscarLotesAtualizar = async ({ encerrando, tempoEntreRequisicoes, filtroHoras }) => {
-    const listaBanco = await buscarLista({
-      colecao: 'palacioDosLeiloes',
-      encerrando,
-      filtraEncerrados: true,
-      filtroHoras
     });
 
-    baixarLoteAtualizar(listaBanco, 0, tempoEntreRequisicoes);
+    if (response === '0') {
+      return { encerrado: true };
+    }
+
+    const $ = cheerio.load(response);
+
+    const obj = {
+      fotos: [],
+      lances: [],
+    };
+
+    const totalFotos = $('input#total_fotos').attr('value');
+    for (let idx = 0; idx < totalFotos; idx++) {
+      const foto = $(`div#fl_${idx} img`).attr('src').split('?')[0];
+
+      obj.fotos.push(foto);
+    }
+    obj.lanceAtual = tratarDinheiro($('div.row div.col div.h1').text());
+
+    const tableLances = $('table.table.table-bordered.table-sm.small.mb-1 tr');
+    const tdsHeader = $(tableLances[0]).find('td');
+    obj.totalLances = Number(pegarDeParenteses($(tdsHeader[0]).text()));
+    obj.totalVisualizacoes = Number(pegarDeParenteses($(tdsHeader[1]).text()));
+
+    for (let idx = 1; idx < tableLances.length; idx++) {
+      const tdsLance = $(tableLances[idx]).find('td');
+      const valor = tratarDinheiro($(tdsLance[0]).text());
+      const data = $(tdsLance[2]).text();
+
+      obj.lances.push({ data, valor });
+    }
+
+    obj.lances.reverse();
+
+    const tableInformacoes = $('table.table.table-sm.table-bordered.small.mt-1 tr');
+
+    for (let idx = 0; idx < tableInformacoes.length; idx++) {
+      const tds = $(tableInformacoes[idx]).find('td');
+
+      if (tds.length === 2) {
+        let nome = $(tds[0]).text().replace(/ /g, "");
+        const valor = $(tds[1]).text().trim();
+
+        if (nome.includes('ncia/Cilin')) {
+          nome = 'Potencia_Cilindradas';
+        } else if (nome.includes('Combust')) {
+          nome = 'Combustivel';
+        } else if (nome.includes('Situa')) {
+          nome = 'Situacao';
+        } else if (nome.includes('Previs')) {
+          nome = 'Previsao';
+        } else if (nome.includes('Acess')) {
+          nome = 'Acessorios';
+        } else if (nome.includes('Localleil')) {
+          nome = 'LocalLeilao';
+        } else if (nome === 'Origem') {
+          nome = 'origemTipo';
+        }
+
+        if (nome === 'Previsao') {
+          /*obj.previsao = tratarDataHora(valor);*/
+        } else if (nome === 'Lote') {
+          obj.lote = Number(valor);
+        } else if (!nome.indexOf('Leil') !== 0) {
+          obj[nome] = valor;
+        }
+      }
+    }
+
+    return obj;
+  };
+
+
+  const verificarEncerrando = async (registro) => {
+    const dados = {};
+    const { encerrado, original: o } = await get({ colecao, site, registro });
+
+    if (encerrado === true && o.encerrado === true) {
+      console.log(registro, 'Registro já encerrado');
+    } else if (!isNaN(o.encerrado)) {
+      dados['original.encerrado'] = ++o.encerrado;
+    } else {
+      dados['original.encerrado'] = 1;
+    }
+
+    if (dados['original.encerrado'] > 5) {
+      dados.encerrado = true;
+      dados['original.encerrado'] = true;
+    }
+
+    return dados;
   };
 
   const baixarLoteAtualizar = async (array, index, tempoEntreRequisicoes) => {
-    const collection = 'palacioDosLeiloes';
-
     if (index > array.length - 1) {
       console.log(`Fim da lista - ${index+1}/${array.length}`);
 
@@ -104,12 +184,14 @@ const exec = (params) => {
     console.log(registro, `${index+1}/${array.length}`, 'Vamos atualizar');
 
     try {
-      const informacoesSite = await verLotes(registro);
+      const informacoesSite = await baixarPagina(registro);
+      const dados = informacoesSite.encerrado
+        ? verificarEncerrando(registro)
+        : dadosItem(informacoesSite);
 
-      await atualizarRegistro(collection, {
-        registro,
-        ...informacoesSite
-      });
+      dados.registro = registro;
+
+      await salvarLista([dados]);
     } catch (e) {
       console.log(registro, 'Erro na atualização', e);
     }
@@ -117,7 +199,43 @@ const exec = (params) => {
     setTimeout(() => baixarLoteAtualizar(array, index+1, tempoEntreRequisicoes), tempoEntreRequisicoes);
   };
 
-  return buscarLotesAtualizar;
+  const fnc = async ({ encerrando, tempoEntreRequisicoes, filtroHoras }) => {
+    const data = new Date();
+    const filtro = {
+      site,
+      'original.encerrado': encerrando ? {$gte: 1} : {$ne: true}
+    };
+
+    if (filtroHoras == '30') {
+      data.setTime(data.getTime() + (30 * 60 * 1000));
+      filtro['original.previsao.time'] = {$lt: data.getTime()};
+    } else if (filtroHoras == '2') {
+      data.setTime(data.getTime() + (30 * 60 * 1000));
+      const inicial = data.getTime();
+
+      data.setTime(data.getTime() + (120 * 60 * 1000));
+      const final = data.getTime();
+
+      filtro['original.previsao.time'] = {$gte: inicial, $lt: final};
+    } else if (filtroHoras == '6') {
+      data.setTime(data.getTime() + (120 * 60 * 1000));
+      const inicial = data.getTime();
+
+      data.setTime(data.getTime() + (240 * 60 * 1000));
+      const final = data.getTime();
+
+      filtro['original.previsao.time'] = {$gte: inicial, $lt: final};
+    } else if (filtroHoras == '+6') {
+      data.setTime(data.getTime() + (240 * 60 * 1000));
+      filtro['$or'] = [{'original.previsao.time': {$gte: data.getTime()}}, {'original.previsao.string': ''}];
+    }
+
+    const listaBanco = await list({ colecao, filtro, colunas: { registro: true } });
+
+    baixarLoteAtualizar(listaBanco, 0, tempoEntreRequisicoes);
+  };
+
+  return fnc;
 };
 
 export default exec;
--- sites/palaciodosleiloes/buscarLotesSalvar.js
+++ sites/palaciodosleiloes/buscarLotesSalvar.js
@@ -1,44 +1,154 @@
-import ll from './listarLotes.js';
+import tratarDataHora from './_tratarDataHora.js';
 
-const exec = (params) => {
-  const listarLotes = ll(params);
-  const { buscarLista, insert, update } = params.db;
+const exec = ({ cheerio, request, db: { salvarLista } }) => {
+  const dadosItem = (dados) => {
+    const {
+      registro,
+      encerrado,
+      bem: veiculo,
+      origem: vendedor,
+      Locallote: localLote,
+      LocalLeilao: localLeilao,
+      realizacao: dataInicio,
+      Acessorios,
+      Ano: ano,
+      Combustivel: combustivel,
+      Situacao: situacao,
+      lances,
+      origemTipo: vendedorTipo,
+      KM,
+      descricao,
+      fotos,
+    } = dados;
 
-  const salvarLista = async (listaSite) => {
-    const colecao = 'palacioDosLeiloes';
-    const listaBanco = await buscarLista({ colecao });
+    const {
+      data: ultimoLanceData,
+      valor: ultimoLanceValor
+    } = (lances || [{}]).pop();
 
-    listaSite.forEach(async (i, index, array) => {
-      const itemBanco = listaBanco.find(({ registro }) => registro.lote === i.registro.lote && registro.leilao === i.registro.leilao);
+    const retorno = {};
+    const objeto = {
+      registro,
+      vendedor,
+      vendedorTipo,
+      veiculo,
+      combustivel,
+      ano,
+      km: isNaN(KM) ? KM : Number(KM.replace('.', '')),
+      situacao,
+      acessorios: (Acessorios || '').split(','),
+      descricao,
+      fotos,
+      ultimoLanceData,
+      ultimoLanceValor,
+      localLote,
+      localLeilao,
+      dataInicio,
+      encerrado,
+      original: dados
+    };
 
-      if (itemBanco) {
-        const set = {};
-
-        if (i.previsao.time && JSON.stringify(i.previsao) != JSON.stringify(itemBanco.previsao)) {
-          set.previsao = i.previsao;
-        }
-
-        if (i.sequencia && JSON.stringify(i.sequencia) != JSON.stringify(itemBanco.sequencia)) {
-          set.sequencia = i.sequencia;
-        }
-
-        if (i.lote && JSON.stringify(i.lote) != JSON.stringify(itemBanco.lote)) {
-          set.lote = i.lote;
-        }
-
-        if (JSON.stringify(set) != '{}') {
-          const atualizado = await update({ colecao, registro: i.registro, set });
-
-          console.log(colecao, i.registro, `Registro ${atualizado ? '' : 'não '}atualizado`);
-        } else {
-          console.log(colecao, i.registro, 'Registro já cadastrado');
-        }
-      } else {
-        const id = await insert({ colecao, dados: i });
-
-        console.log(colecao, i.registro, `${id ? '' : 'Não '}Cadastrado`, id);
+    Object.entries(objeto).forEach(([key, value]) => {
+      if (value) {
+        retorno[key] = value;
       }
     });
+
+    return retorno;
+  };
+
+  const listarLotes = async () => {
+    const response = await request.post('https://www.palaciodosleiloes.com.br/camada_ajax/lotes.php', {
+      form: {
+        opcao: "busca_lotes",
+        categoria_pesquisa: "1",
+        tipo_exibicao_pesquisa: "lista"
+      }
+    });
+
+    const $ = cheerio.load(response);
+    const lista = [];
+
+    $('tr').each((index, tr) => {
+      const onclick = $(tr).attr('onclick');
+
+      if (!onclick) {
+        return;
+      }
+
+      const dado = {
+        registro: {
+          lote: Number(onclick.substring(onclick.indexOf('(')+1, onclick.indexOf(','))),
+          leilao: Number(onclick.substring(onclick.indexOf(',')+1, onclick.indexOf(')')))
+        }
+      };
+
+      const divs = $(tr).find('div');
+      if (divs.length === 20) {
+        dado.sequencia = $(divs[1]).text().indexOf("Sequ") > -1 ? Number($(divs[2]).text()) : null;
+        dado.lote = $(divs[3]).text() === "Lote" ? $(divs[4]).text() : null;
+        dado.local = $(divs[5]).text() === "Local" ? $(divs[6]).text() : null;
+        dado.totalVisualizacoes = $(divs[7]).text().indexOf("Visualiza") > -1 ? $(divs[8]).text() : null;
+        dado.totalLances = $(divs[9]).text() === "Lances" ? $(divs[10]).text() : null;
+        dado.realizacao = $(divs[11]).text().indexOf("Realiza") > -1 ? tratarDataHora($(divs[12]).text()) : null;
+        dado.previsao = $(divs[13]).text().indexOf("Previs") > -1 ? tratarDataHora($(divs[14]).text()) : null;
+        dado.ultimoLance = $(divs[15]).text().indexOf("ltimo") > 0 ? $(divs[16]).text() : null;
+        dado.bem = $(divs[17]).text();
+        dado.origem = $(divs[18]).text();
+        dado.descricao = $(divs[18]).text();
+      } else if (divs.length === 18) {
+        dado.lote = $(divs[1]).text() === "Lote" ? $(divs[2]).text() : null;
+        dado.local = $(divs[3]).text() === "Local" ? $(divs[4]).text() : null;
+        dado.totalVisualizacoes = $(divs[5]).text().indexOf("Visualiza") > -1 ? $(divs[6]).text() : null;
+        dado.totalLances = $(divs[7]).text() === "Lances" ? $(divs[8]).text() : null;
+        dado.realizacao = $(divs[9]).text().indexOf("Realiza") > -1 ? tratarDataHora($(divs[10]).text()) : null;
+        dado.previsao = $(divs[11]).text().indexOf("Previs") > -1 ? tratarDataHora($(divs[12]).text()) : null;
+        dado.ultimoLance = $(divs[13]).text().indexOf("ltimo") > 0 ? $(divs[14]).text() : null;
+        dado.bem = $(divs[15]).text();
+        dado.origem = $(divs[16]).text();
+        dado.descricao = $(divs[17]).text();
+      }
+
+      if (dado.previsao.string && !dado.previsao.time && dado.previsao.string.includes(':') && dado.realizacao.date) {
+        dado.previsao = tratarDataHora(`${dado.realizacao.string} ${dado.previsao.string}`);
+      } else if (dado.previsao.string && dado.previsao.string.length < 5) {
+        dado.previsao = dado.realizacao;
+      }
+
+      if (!isNaN(dado.sequencia)) {
+        dado.sequencia = Number(dado.sequencia);
+      }
+
+      if (!isNaN(dado.lote)) {
+        dado.lote = Number(dado.lote);
+      }
+
+      if (!isNaN(dado.totalVisualizacoes)) {
+        dado.totalVisualizacoes = Number(dado.totalVisualizacoes);
+      }
+
+      if (!isNaN(dado.totalLances)) {
+        dado.totalLances = Number(dado.totalLances);
+      }
+
+      if (dado.ultimoLance) {
+        const val = dado.ultimoLance.replace('.', '').replace(',', '.');
+
+        if (!isNaN(val)) {
+          dado.ultimoLance = Number(val);
+        }
+      }
+
+      if (dado.descricao.toLowerCase().indexOf('colis') === 0) {
+        dado.tipo = 'colisao';
+      } else if (dado.descricao.toLowerCase().indexOf('furto') === 0 || dado.descricao.toLowerCase().indexOf('roubo') === 0) {
+        dado.tipo = 'roubo'
+      }
+
+      lista.push(dadosItem(dado));
+    });
+
+    return lista;
   };
 
   const fnc = async () => {
