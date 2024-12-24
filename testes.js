b37c98835a05a1a719494dcc46f5b6d643b514cb
Bruno Tuy <bruno.tuy@pixeon.com>
2024-12-23 23:26:35 -0300

    teste file

---
+++ testes.js
@@ -0,0 +1,15 @@
+import request from 'request-promise';
+import cheerio from 'cheerio';
+import p from './sites/palaciodosleiloes/index.js';
+import v from './sites/vipleiloes/index.js';
+import m from './sites/milanleiloes/index.js';
+import g from './sites/guariglialeiloes/index.js';
+import _db from './db.js';
+
+const db = await _db();
+const palacio = await p({ cheerio, request, db });
+const vip = await v({ cheerio, request, db });
+const milan = await m({ cheerio, request, db });
+const guariglia = await g({ cheerio, request, db });
+
+guariglia.buscarListaPrincipal();
